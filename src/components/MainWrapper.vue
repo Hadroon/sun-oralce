a75ecd8311bd38f0b47e83d68557ff25fe2bcdd4
gabormuranyi-planorama <gabor.muranyi@planorama.com>
2019-02-05 14:31:19 +0100

    first design

--- src/components/MainWrapper.vue
+++ src/components/MainWrapper.vue
@@ -1,5 +1,69 @@
 <template>
-    <p>Hello from mainWrapper</p>
+  <div class="mainWrapper">
+    <div id="first">
+      <img src="/images/head3.gif" alt="">
+      <img class="spinner" src="/images/turbine.png" alt="">
+      <img class="logo" src="/images/logo.jpg" alt="">
+      <div class="headText">
+
+        <h1>
+          Te mennyire vagy <span class="brown">környezettudatos</span>?
+        </h1>
+
+        <h3>
+          Válaszaiddal most <span class="brown">500.000 Ft értékű</span> vásárlási <span class="brown">utalványt nyerhetsz.</span><br>
+
+          Töltsd ki <span class="brown">pár perc</span>es kérdőívünket és <span class="brown">hasznos tippekkel látunk el</span><br> hogy hogyan tudnád élhetőbbé tenni a környezetedet.
+
+        </h3>
+        <h1>
+          Éljen a <span class="brown">Föld</span>!  - A <span class="brown">Földdel</span> együtt Te is nyerhetsz!
+        </h1>
+
+        <p>Sorsolás: 2019.04.22.</p>
+      </div>
+    </div>
+    <div id="second">
+      <div class="price">
+        <img src="/images/price1.png" alt="">
+      </div>
+      <div class="price">
+        <img src="/images/price2.png" alt="">
+      </div>
+      <div class="price">
+        <img src="/images/price3.png" alt="">
+      </div>
+      <img class="separator" src="/images/grass.png" alt="">
+    </div>
+    <div id="third">
+      <form class="question" action="">
+        <h2>3 km-nél kisebb távolságra…</h2>
+        <input type="radio" name="gender" value="male"> Autóba pattanok, gyorsabb így.<br>
+        <input type="radio" name="gender" value="female"> Tömegközlekedem, úgyis jön valami.<br>
+        <input type="radio" name="gender" value="other"> Gyalogolok vagy bringázom.
+      </form>
+      <form class="question" action="">
+        <h2>3 km-nél kisebb távolságra…</h2>
+        <input type="radio" name="gender" value="male"> Autóba pattanok, gyorsabb így.<br>
+        <input type="radio" name="gender" value="female"> Tömegközlekedem, úgyis jön valami.<br>
+        <input type="radio" name="gender" value="other"> Gyalogolok vagy bringázom.
+      </form>
+      <form class="question" action="">
+        <h2>3 km-nél kisebb távolságra…</h2>
+        <input type="radio" name="gender" value="male"> Autóba pattanok, gyorsabb így.<br>
+        <input type="radio" name="gender" value="female"> Tömegközlekedem, úgyis jön valami.<br>
+        <input type="radio" name="gender" value="other"> Gyalogolok vagy bringázom.
+      </form>
+      <form class="question" action="">
+        <h2>3 km-nél kisebb távolságra…</h2>
+        <input type="radio" name="gender" value="male"> Autóba pattanok, gyorsabb így.<br>
+        <input type="radio" name="gender" value="female"> Tömegközlekedem, úgyis jön valami.<br>
+        <input type="radio" name="gender" value="other"> Gyalogolok vagy bringázom.
+      </form>
+
+
+    </div>
+  </div>
 </template>
 
 <script>
