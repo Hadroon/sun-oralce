6bbae90dfac6720af045c70458ecb985be6cbbc2
gabormuranyi-planorama <gabor.muranyi@planorama.com>
2019-02-14 09:22:11 +0100

    https v1

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
+var https = require('https');
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+
 app = express();
+
+app.use((req, res, next) => {
+    console.log(req.header('x-forwarded-proto'));
+    if (req.header('x-forwarded-proto') !== 'https') {
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    } else {
+      next();
+    }
+  });
+
 app.use(serveStatic(__dirname + "/dist"));
+
 var port = process.env.PORT || 8080;
+
 app.listen(port);
 console.log('server started '+ port);
